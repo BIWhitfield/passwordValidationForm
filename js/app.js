d407a8d246b371507b89fef1909eb979b30d243f
BIWhitfield <beniwhitfield@gmail.com>
2017-08-26 14:40:58 +0100

    [app.js] add button disabled when password too weak, capitals added to regex, re-enable button when password strong

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+document.getElementById("submit-button").disabled = true;
+
 const checkForm = (form) => {
     if (form.username.value === "") {
     alert("Error: Username cannot be blank!");
@@ -17,7 +19,7 @@ const checkPassword = (pwd) => {
   if (typeof pwd.value === 'string') {
     document.getElementById("password-result").innerHTML = null;
     // Length less than 4
-    if (pwd.value.length < 4 && /^[a-z0-9!@£#$%^&*]/i.test(pwd.value)) {
+    if (pwd.value.length < 4) {
       let p = document.createElement('p');
       p.appendChild(document.createTextNode('Weak - Too short'))
       document.getElementById('password-result').appendChild(p);
@@ -26,9 +28,9 @@ const checkPassword = (pwd) => {
     }
 
     // Length less than 9 all letters
-    if (pwd.value.length < 9 && /^[a-z0-9!@£#$%^&*]/i.test(pwd.value)) {
+    if (pwd.value.length < 9) {
       let p = document.createElement('p');
-      p.appendChild(document.createTextNode('Weak'))
+      p.appendChild(document.createTextNode('Weak - Try mixing characters'))
       document.getElementById('password-result').appendChild(p);
       pwd.focus();
       return false
@@ -37,7 +39,7 @@ const checkPassword = (pwd) => {
     // Length more than 8, less than 12, all letters
     if (/^[a-z]{9,12}$/i.test(pwd.value)) {
       let p = document.createElement('p');
-      p.appendChild(document.createTextNode('Weak'))
+      p.appendChild(document.createTextNode('Weak - Try mixing characters'))
       document.getElementById('password-result').appendChild(p);
       pwd.focus();
       return false
@@ -46,26 +48,27 @@ const checkPassword = (pwd) => {
     // Length more than 8, less than 12, letters and numbers
     if (/^[a-z0-9]{9,12}$/i.test(pwd.value)) {
       let p = document.createElement('p');
-      p.appendChild(document.createTextNode('Less Weak! - Try mixing numbers, letters and special characters'))
+      p.appendChild(document.createTextNode('Mix capitals, special characters and increase length'))
       document.getElementById('password-result').appendChild(p);
       pwd.focus();
       return false
     }
 
-    // Length more than 8, less than 12, letters, numbers and special characters
-    if (/^[a-z0-9!@£#$%^&*]{9,12}$/i.test(pwd.value)) {
+    // Length more than 8, less than 12, letters, capitals, numbers and special characters
+    if (/^[a-z0-9A-Z!@£#$%^&*]{9,12}$/i.test(pwd.value)) {
       let p = document.createElement('p');
-      p.appendChild(document.createTextNode('Medium - Try extending length'))
+      p.appendChild(document.createTextNode('OK - Try extending length'))
       document.getElementById('password-result').appendChild(p);
       pwd.focus();
       return false
     }
 
     // Length more than 12, letters, numbers and special characters
-    if (/^[a-z0-9!@£#$%^&*]{13,}$/i.test(pwd.value)) {
+    if (/^[a-zA-Z0-9!@£#$%^&*]{13,}$/i.test(pwd.value)) {
       let p = document.createElement('p');
       p.appendChild(document.createTextNode('Strong'))
       document.getElementById('password-result').appendChild(p);
+      document.getElementById("submit-button").disabled = false;
       pwd.focus();
       return false
     }
